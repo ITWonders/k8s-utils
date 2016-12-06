261811ecd82c553d822b0bdcbdccdbed9c1c04cd
Ken Ng <kenngsimply@gmail.com>
2016-12-06 14:46:16 +0800

    refactor

--- kushell.go
+++ kushell.go
@@ -6,29 +6,38 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 	"syscall"
 )
 
+var kind string
+var mycommand []string
+
 func main() {
-	kushell()
+	kind = "po"
+	mycommand = []string{"kubectl", "exec", "-it"}
+
+	if len(os.Args) > 1 {
+		kind = os.Args[1]
+		mycommand = []string{"gcloud", "compute", "ssh"}
+		gcloudssh()
+	} else {
+		kushell()
+	}
 }
 
-func kushell() {
+func kuget() ([]byte, error) {
 	session := sh.NewSession()
-
-	lines, err := session.Command("kubectl", "get", "po", "-o", "wide").
+	return session.Command("kubectl", "get", kind, "-o", "wide").
 		Command("awk", `{ print $1 }`).
 		Command("tail", "-n", "+2").
 		Output()
+}
 
-	if err != nil {
-		fmt.Println("error: ", err)
-		return
-	}
-
+func getUserChoice(output string) string {
 	// split the result based on newline
 	re := regexp.MustCompile(`\n`)
-	result := re.Split(string(lines), -1)
+	result := re.Split(output, -1)
 
 	for k, v := range result {
 		fmt.Printf("%d: %s\n", k, v)
@@ -37,9 +46,44 @@ func kushell() {
 	var chosen int
 	fmt.Scanf("%d", &chosen)
 
+	return result[chosen]
+}
+
+func kushell() {
+	lines, err := kuget()
+
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+
+	selected := getUserChoice(string(lines))
+	mycommand := append(mycommand, selected, "--", "/bin/sh")
+
+	fmt.Println(strings.Join(mycommand, " "))
 	binary, _ := exec.LookPath("kubectl")
 	syscall.Exec(
 		binary,
-		[]string{"kubectl", "exec", "-it", result[chosen], "--", "/bin/sh"},
+		mycommand,
 		os.Environ())
+}
+
+func gcloudssh() {
+	lines, err := kuget()
+
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+
+	selected := getUserChoice(string(lines))
+	mycommand := append(mycommand, selected)
+
+	fmt.Println(strings.Join(mycommand, " "))
+	binary, _ := exec.LookPath("gcloud")
+	syscall.Exec(
+		binary,
+		mycommand,
+		os.Environ())
+
 }
